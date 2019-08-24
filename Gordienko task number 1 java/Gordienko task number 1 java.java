7cc8ccad47d4eec027994dc87e2e6a283c542dff
DarknesS3 <gordienko.a.a.q@gmail.com>
2019-08-24 15:14:24 +0300

    1

--- Gordienko task number 1 java/Gordienko task number 1 java.java
+++ Gordienko task number 1 java/Gordienko task number 1 java.java
@@ -1,12 +1,15 @@
+/* Create an algorithm: if the entered number is greater than 7, then print “Hello”
+*/
+
 import java.util.Scanner;
 
  public class Hello {
 	 public static void main(String args[]){
 		 Scanner inputNumber = new Scanner(System.in);
-	        System.out.print("Введите число: ");
+	        System.out.print("Enter number: ");
 	        if (inputNumber.nextInt() > 7 ) 
-	            System.out.println("Привет");
+	            System.out.println("Hello");
 	        else 
-	            System.out.println("До свидания");
+	            System.out.println("Good Bye");
 	 }
 }
